de1869a9568d928a97f7dae3ac6cdd4b146606a3
Nate Lampros <natelamp@byu.edu>
2024-02-16 13:51:35 -0700

    register presenter done

--- tweeter-web/src/model/service/UserService.ts
+++ tweeter-web/src/model/service/UserService.ts
@@ -1,4 +1,5 @@
 import { AuthToken, User, FakeData } from "tweeter-shared";
+import { Buffer } from "buffer";
 
 export class UserService {
   public async login(
@@ -10,6 +11,27 @@ export class UserService {
 
     if (user === null) {
       throw new Error("Invalid alias or password");
+    }
+
+    return [user, FakeData.instance.authToken];
+  }
+
+  public async register(
+    firstName: string,
+    lastName: string,
+    alias: string,
+    password: string,
+    userImageBytes: Uint8Array
+  ): Promise<[User, AuthToken]> {
+    // Not neded now, but will be needed when you make the request to the server in milestone 3
+    let imageStringBase64: string =
+      Buffer.from(userImageBytes).toString("base64");
+
+    // TODO: Replace with the result of calling the server
+    let user = FakeData.instance.firstUser;
+
+    if (user === null) {
+      throw new Error("Invalid registration");
     }
 
     return [user, FakeData.instance.authToken];
